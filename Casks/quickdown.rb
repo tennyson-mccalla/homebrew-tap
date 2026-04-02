0910f3718d517bc32b7b6749213ead60353efbd3
tennyson-mccalla <tennyson.mccalla@gmail.com>
2026-04-01 19:33:00 -0500

    Update QuickDown to v0.6.0

--- Casks/quickdown.rb
+++ Casks/quickdown.rb
@@ -1,15 +1,15 @@
 cask "quickdown" do
-  version "0.5.1"
-  sha256 "cce5b45c45d41129b1c73534aabe5baa43edc3fb4faa996f9af2e8aa562e118d"
+    version "0.6.0"
+    sha256 "2460582de0d9e89c60bca7d1c755091ed79f18a7f6a1479223d131203403ddf1"
 
-  url "https://github.com/tennyson-mccalla/QuickDown/releases/download/v#{version}/QuickDown.dmg"
-  name "QuickDown"
-  desc "Fast, native Markdown previewer with QuickLook integration"
-  homepage "https://github.com/tennyson-mccalla/QuickDown"
+    url "https://github.com/tennyson-mccalla/QuickDown/releases/download/v#{version}/QuickDown.dmg"
+    name "QuickDown"
+    desc "Fast, native Markdown previewer with QuickLook integration"
+    homepage "https://github.com/tennyson-mccalla/QuickDown"
 
-  depends_on macos: ">= :sonoma"
+    depends_on macos: ">= :sonoma"
 
-  app "QuickDown.app"
+    app "QuickDown.app"
 
-  zap trash: "~/Library/Preferences/com.tennyson.QuickDown.plist"
-end
+    zap trash: "~/Library/Preferences/com.tennyson.QuickDown.plist"
+  end
